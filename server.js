12d133c70af46b5aef6f1b0d8f30da191305b6bd
LENOVO <lchetam2002@gmail.com>
2022-09-14 19:35:06 +0530

    updated port

--- server.js
+++ server.js
@@ -34,7 +34,10 @@ app.post("/", function (req, res) {
   newForm.save();
   res.redirect("/main");
 });
-
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+app.listen(port, function () {
   console.log("server is running");
 });
